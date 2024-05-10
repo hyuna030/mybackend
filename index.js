d14dc00bcfc3ca166664489c35df08c250c3b51b
hyuna030 <jhacomba1@gmail.com>
2024-05-11 06:59:44 +0900

    vercel.json add

--- index.js
+++ index.js
@@ -26,37 +26,9 @@ const storage = new Storage({
 });
 const bucket = storage.bucket(process.env.FIREBASE_STORAGE_BUCKET);
 
-const app = express();
-
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', 'https://finalproject-puce.vercel.app/')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
-
-const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-
+const app = express(); f
 app.use(cors());
 app.use(express.json());
-
-
-
 
 
 
